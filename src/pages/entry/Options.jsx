def80dbfc26bd55bf0f5266b649d8121002194dc
KalPal14 <reznkola@gmail.com>
2022-09-29 18:59:02 +0300

    test/for logic of selecting Scoops and Toppings

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,11 +1,11 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import Row from 'react-bootstrap/Row';
-import ScoopOption from './ScoopOption';
-import ToppingOption from './ToppingOption';
-import AlertBanner from '../common/AlertBanner';
-import { pricePerItem } from '../../constants';
-import { useOrderDetails } from '../../contexts/OrderDetails';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Row from "react-bootstrap/Row";
+import ScoopOption from "./ScoopOption";
+import ToppingOption from "./ToppingOption";
+import AlertBanner from "../common/AlertBanner";
+import { pricePerItem } from "../../constants";
+import { useOrderDetails } from "../../contexts/OrderDetails";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
@@ -25,7 +25,7 @@ export default function Options({ optionType }) {
     return <AlertBanner />;
   }
 
-  const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
   const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
 
   const optionItems = items.map((item) => (
@@ -42,7 +42,7 @@ export default function Options({ optionType }) {
   return (
     <>
       <h2>{title}</h2>
-      <p>{pricePerItem[optionType]} each</p>
+      <p>${pricePerItem[optionType]} each</p>
       <p>
         {title} total: {orderDetails.totals[optionType]}
       </p>
--- src/pages/entry/Options.test.jsx
+++ src/pages/entry/Options.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from "../../test-utils/testing-library-utils";
+import userEvent from "@testing-library/user-event";
 import Options from "./Options";
 
 describe("Options", () => {
@@ -26,5 +27,78 @@ describe("Options", () => {
       "M&Ms topping",
       "Hot fudge topping",
     ]);
+  });
+
+  test("Scoops total is 0.00 by default", () => {
+    render(<Options optionType="scoops" />);
+    const scoopsTotal = screen.getByText(/Scoops total:/i);
+
+    expect(scoopsTotal).toHaveTextContent("0.00");
+  });
+
+  test("Scoops total when chouse one scoop", async () => {
+    render(<Options optionType="scoops" />);
+    const scoopsTotal = screen.getByText(/Scoops total:/i);
+    const chocolateScoopAmountInput = await screen.findByRole("spinbutton", {
+      name: /Chocolate/i,
+    });
+
+    await userEvent.clear(chocolateScoopAmountInput);
+    await userEvent.type(chocolateScoopAmountInput, "1");
+
+    expect(scoopsTotal).toHaveTextContent("2.00");
+  });
+
+  test("Scoops total when chouse several scoops", async () => {
+    render(<Options optionType="scoops" />);
+    const scoopsTotal = screen.getByText(/Scoops total:/i);
+    const chocolateScoopAmountInput = await screen.findByRole("spinbutton", {
+      name: /Chocolate/i,
+    });
+    const vanillaScoopAmountInput = await screen.findByRole("spinbutton", {
+      name: /Vanilla/i,
+    });
+
+    await userEvent.clear(chocolateScoopAmountInput);
+    await userEvent.type(chocolateScoopAmountInput, "1");
+    await userEvent.clear(vanillaScoopAmountInput);
+    await userEvent.type(vanillaScoopAmountInput, "2");
+
+    expect(scoopsTotal).toHaveTextContent("6.00");
+  });
+
+  test("Toppings total is 0.00 by default", () => {
+    render(<Options optionType="toppings" />);
+    const toppingsTotal = screen.getByText(/Toppings total:/i);
+
+    expect(toppingsTotal).toHaveTextContent("0.00");
+  });
+
+  test("Toppings total when chouse one topping", async () => {
+    render(<Options optionType="toppings" />);
+    const toppingsTotal = screen.getByText(/Toppings total:/i);
+    const cherriesToppingCheckbox = await screen.findByRole("checkbox", {
+      name: /Cherries/i,
+    });
+
+    await userEvent.click(cherriesToppingCheckbox);
+
+    expect(toppingsTotal).toHaveTextContent("1.50");
+  });
+
+  test("Toppings total when chouse several toppings", async () => {
+    render(<Options optionType="toppings" />);
+    const toppingsTotal = screen.getByText(/Toppings total:/i);
+    const cherriesToppingCheckbox = await screen.findByRole("checkbox", {
+      name: /Cherries/i,
+    });
+    const hotFudgeToppingCheckbox = await screen.findByRole("checkbox", {
+      name: /Hot fudge/i,
+    });
+
+    await userEvent.click(cherriesToppingCheckbox);
+    await userEvent.click(hotFudgeToppingCheckbox);
+
+    expect(toppingsTotal).toHaveTextContent("3.00");
   });
 });
