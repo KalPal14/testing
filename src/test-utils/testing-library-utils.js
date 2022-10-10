e0eb623df343e351fbb813212e2849c4a1a6e7e5
KalPal14 <reznkola@gmail.com>
2022-10-10 18:08:46 +0300

    setup + test/add one provider to render wrapper

--- src/test-utils/testing-library-utils.js
+++ src/test-utils/testing-library-utils.js
@@ -1,8 +1,17 @@
 import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import { OrderDetailsProvider } from "../contexts/OrderDetails";
 
+const AllProviders = ({ children }) => {
+  return (
+    <OrderDetailsProvider>
+      <MemoryRouter>{children}</MemoryRouter>
+    </OrderDetailsProvider>
+  );
+};
+
 const customRender = (ui, options) =>
-  render(ui, { wrapper: OrderDetailsProvider, ...options });
+  render(ui, { wrapper: AllProviders, ...options });
 
 // re-export everything
 export * from "@testing-library/react";
