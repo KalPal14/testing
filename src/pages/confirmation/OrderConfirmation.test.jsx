3412f022353a566fe80a2b14063e41b26c423203
KalPal14 <reznkola@gmail.com>
2022-10-19 17:00:36 +0300

    test/add test case to OrderConfirmation.test

--- src/pages/confirmation/OrderConfirmation.test.jsx
+++ src/pages/confirmation/OrderConfirmation.test.jsx
@@ -3,12 +3,17 @@ import {
   screen,
   waitFor,
 } from "../../test-utils/testing-library-utils";
+import userEvent from "@testing-library/user-event";
 import { rest } from "msw";
 import { server } from "../../mocks/server";
 import OrderConfirmation from "./OrderConfirmation";
+import * as orderDetailsContext from "../../contexts/OrderDetails";
 import { ORDER_NUMBER } from "../../constants/api";
 
 describe("OrderConfirmation", () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
   test("checking render when we have normal server response", async () => {
     render(<OrderConfirmation />);
     await waitFor(async () => {
@@ -16,6 +21,20 @@ describe("OrderConfirmation", () => {
 
       expect(orderNumber).toHaveTextContent(ORDER_NUMBER);
     });
+  });
+
+  test("checking create new order button", async () => {
+    const mockUseOrderDetails = jest
+      .spyOn(orderDetailsContext, "useOrderDetails")
+      .mockReturnValue([null, null, jest.fn()]);
+    render(<OrderConfirmation />);
+    const newOrderButton = screen.getByRole("button", {
+      name: /Create new order/i,
+    });
+    const mockResetOrder = mockUseOrderDetails()[2];
+    await userEvent.click(newOrderButton);
+
+    expect(mockResetOrder).toHaveBeenCalled();
   });
 
   test("checking render when we have error server response", async () => {
