16c8aeb63a452db0b104addeac4007c030efd99f
KalPal14 <reznkola@gmail.com>
2022-10-19 18:02:51 +0300

    new/add logic of disable and eneble order sundae button

--- src/pages/entry/OrderEntry.jsx
+++ src/pages/entry/OrderEntry.jsx
@@ -5,15 +5,17 @@ import { useOrderDetails } from "../../contexts/OrderDetails";
 
 export default function OrderEntry() {
   const [orderDetails] = useOrderDetails();
+
+  const isOrderBtnDisable = orderDetails.totals.scoops === "$0.00";
   return (
     <div>
       <h1>Design Your Sundae!</h1>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
       <h2>Grand total: {orderDetails.totals.grandTotal}</h2>
-      <Link to="summary">
-        <Button>Order Sundae!</Button>
-      </Link>
+      <Button disabled={isOrderBtnDisable}>
+        <Link to="summary">Order Sundae!</Link>
+      </Button>
     </div>
   );
 }
